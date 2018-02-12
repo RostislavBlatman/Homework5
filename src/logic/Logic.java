cc07aef99d2b20c22633013433e2209bbe7bb7ef
Blatman Rostislav <blatmanrostislav@mail.ru>
2018-02-12 17:19:38 +0300

    добавил тернарный оператор

--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -5,11 +5,9 @@ public class Logic {
         Integer[][] matrix = new Integer[n][n];
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                if(j == i || j == n-1-i) {
-                    matrix[i][j] = 1;
 
-                }
-                else matrix[i][j] = 0;
+                matrix[i][j] = j == i || j == n-1-i ? 1 : 0;
+
             }
         }
         for(int i = 0; i < n; i++) {
